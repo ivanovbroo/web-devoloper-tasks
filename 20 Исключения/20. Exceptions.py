430eb4966903e09e57db9aa32f48dcc33a4e9a81
nikhelangelo <ivanovbroo@gmail.com>
2022-12-02 14:43:48 +0300

    Релиз

--- 20 Исключения/20. Exceptions.py
+++ 20 Исключения/20. Exceptions.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+from api import register_booking
 
 
 class Booking:
@@ -9,17 +10,22 @@ class Booking:
             raise ValueError("Начальная дата позже конечной даты")
 
         self.__room_name = room_name
-        self.__start     = start
-        self.__end       = end         
-    
-    start = property()
-    end   = property()
-    
+        self.__start = start
+        self.__end = end
+
+    @property
+    def start(self):
+        return self.__start
+
+    @property
+    def end(self):
+        return self.__end
+
     @start.setter
     def start(self, start_date):
         if start_date > self.__end:
             raise ValueError("Начальная дата позже конечной даты")
-            
+
         self.__start = start_date
 
     @end.setter
@@ -35,11 +41,11 @@ class Booking:
     @property
     def duration(self):
         return float((self.__end - self.__start).total_seconds()//60)
-    
+
     @property
     def start_date(self):
         return self.__start.strftime("%Y-%m-%d")
-    
+
     @property
     def start_time(self):
         return self.__start.strftime("%H:%M")
@@ -47,42 +53,41 @@ class Booking:
     @property
     def end_date(self):
         return self.__end.strftime("%Y-%m-%d")
-    
+
     @property
     def end_time(self):
         return self.__end.strftime("%H:%M")
 
-    def create_booking(room_name, start, end):
-        print("Начинаем создание бронирования")
-        booking = Booking(room_name, start, end)
-        date_booking = dict()
-        try:            
-            if register_booking(booking) == True:
-                date_booking["created"] = True
-                date_booking["msg"]     = "Бронирование создано"
-            else:
-                date_booking["created"] = False
-                date_booking["msg"]     = "Комната занята"
-
-        except KeyError:
+def create_booking(room_name, start, end) -> str:
+    print("Начинаем создание бронирования")
+    booking = Booking(room_name, start, end)
+    date_booking = dict()
+    try:
+        if register_booking(booking) == True:
+            date_booking["created"] = True
+            date_booking["msg"] = "Бронирование создано"            
+        else:
             date_booking["created"] = False
-            date_booking["msg"]     = "Комнатане найдена"
-        finally:
-            print("Заканчиваем создание бронирования")
+            date_booking["msg"] = "Комната занята"
+    except KeyError:
+        date_booking["created"] = False
+        date_booking["msg"] = "Комната не найдена"
+    finally:
+        print("Заканчиваем создание бронирования")
 
-        date_booking["booking"] = {"room_name" : booking.room_name,
-                                   "start_date": booking.start_date,
-                                   "start_time": booking.start_time,
-                                   "end_date"  : booking.end_date,
-                                   "end_time"  : booking.end_time,
-                                   "duration"  : booking.duration}
+    date_booking["booking"] = {"room_name" : booking.room_name,
+                               "start_date": booking.start_date,
+                               "start_time": booking.start_time,
+                               "end_date"  : booking.end_date,
+                               "end_time"  : booking.end_time,
+                               "duration"  : booking.duration}
 
-        return json.dumps(date_booking)
-
-
+    return json.dumps(date_booking)
 
 
-b = Booking(1, datetime.datetime(2022, 9, 1, 14), datetime.datetime(2022, 9, 1, 15, 15))
+
+b = Booking(1, datetime.datetime(2022, 9, 1, 14),
+            datetime.datetime(2022, 9, 1, 15, 15))
 print(b.__dict__)
 
 print(b.end_date)
@@ -92,4 +97,3 @@ print(b.__dict__)
 
 b.end = datetime.datetime(2022, 9, 1, 16)
 print(b.__dict__)
-
