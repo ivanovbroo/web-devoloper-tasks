a22584fc33e4c2c3dc5c692dfa5a4a06d0c1c656
nikhelangelo <ivanovbroo@gmail.com>
2022-12-03 15:34:07 +0300

    релиз

--- 21 Декораторы 1/Decorators 1.py
+++ 21 Декораторы 1/Decorators 1.py
@@ -1,1 +1,21 @@
-print("H")
+import time
+
+def time_decorator(func):
+    def wrapper():
+        start_time = time.time()
+        result = func()
+        print(f"{round((time.time() - start_time))}")
+        return result
+    return wrapper
+
+
+
+@time_decorator
+def sleep_1_sec():
+    time.sleep(3)
+    print("function")
+    return 25
+
+result = sleep_1_sec()
+
+print(result)
