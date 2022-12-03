c043065cd81b81506fb7e908b30ef55965aa5ccf
nikhelangelo <ivanovbroo@gmail.com>
2022-12-03 15:39:38 +0300

    релиз2

--- 21 Декораторы 1/Decorators 1.py
+++ 21 Декораторы 1/Decorators 1.py
@@ -2,13 +2,11 @@ import time
 
 def time_decorator(func):
     def wrapper():
-        start_time = time.time()
+        start_time = time.monotonic()
         result = func()
-        print(f"{round((time.time() - start_time))}")
+        print(f"{round((time.monotonic() - start_time))}")
         return result
     return wrapper
-
-
 
 @time_decorator
 def sleep_1_sec():
